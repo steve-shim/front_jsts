202df0a38826184154840b0d3c188227563bf3d4
SteveShim <steveshim496@gmail.com>
2023-01-19 06:22:12 +0900

    Add Route Page

--- makeapplication/3_3/sshapp.js
+++ makeapplication/3_3/sshapp.js
@@ -8,6 +8,11 @@ const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
+//여러 함수들에 걸쳐서 접근하는 공유되는 자원들을 하나로 묶어 둠
+const store = {
+    currentPage: 1,
+}
+
 //공통된 코드를 묶는 구조로서 함수사용
 //변수들을 묶는 구조로 객체사용  
 function getData(url) {
@@ -24,24 +29,29 @@ function newsFeed() {
     const newsList = [];
 
     newsList.push('<ul>')
-    for(let i = 0; i < 10; i++) {
+    for(let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
         newsList.push(`
         <li>
-            <a href="#${newsFeed[i].id}">
+            <a href="#/show/${newsFeed[i].id}">
                 ${newsFeed[i].title} (${newsFeed[i].comments_count})
             </a>
         </li>
         `);
     }
     newsList.push('</ul>')
-
+    newsList.push(`
+        <div>
+            <a href="#/page/${store.currentPage > 1 ? store.currentPage - 1 : 1}">이전 페이지</a>
+            <a href="#/page/${store.currentPage + 1}">다음 페이지</a>
+        </div>
+    `);
     container.innerHTML = newsList.join('')
 }
 
 function newsDetail() {
     //location(브라우저가 기본으로 제공해주는 객체: 주소와 관련한 다양한 정보 제공)
     console.log("location.hash",location.hash) // #34403684
-    const id = location.hash.substr(1)
+    const id = location.hash.substr(7)
     
     const newsContent = getData(CONTENT_URL.replace('@id', id))
     const title = document.createElement('h1');
@@ -49,7 +59,7 @@ function newsDetail() {
     container.innerHTML = `
         <h1>${newsContent.title}</h1>
         <div>
-            <a href="#">목록으로</a>
+            <a href="#/page/${store.currentPage}">목록으로</a>
         </div>
     `;
 }
@@ -61,8 +71,14 @@ function router() {
     if (routePath === '') { 
         // location.hash에 #만 들어가는 경우엔 빈값을 반환
         //글 목록
-        newsFeed()
+        newsFeed();
+    } else if (routePath.indexOf('#/page/') >= 0) { 
+        //입력으로 주어진 문자열이 있으면 페이지 화면  
+        //글 목록(페이지 정보 반영)
+        store.currentPage = Number(routePath.substr(7));
+        newsFeed();
     } else {
+        //입력으로 주어진 문자열이 없으면 글 본문 화면  
         //글 본문
         newsDetail()
     }
