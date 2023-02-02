df2cadbf2fd18662e51ac1b041d0b48d38e96158
steve-shim <steveshim496@gmail.com>
2023-02-02 09:49:28 +0900

    add class

--- JavascriptAndTypescript/test_classssh_2.js
+++ JavascriptAndTypescript/test_classssh_2.js
@@ -3,6 +3,10 @@ class CartV2 {
         name, price
     });
 
+    // static createItem = (name, price) => {
+    //     return { name: name, price: price }
+    // };
+
     cart;
     currentId;
 
@@ -30,6 +34,7 @@ class CartV2 {
 }
 
 const shoppingCartV2 = new CartV2();
+// static method는 인스턴스로 접근불가하고 클래스로 접근 가능하다
 console.log(CartV2.createItem('ssh',100));
 shoppingCartV2.addItem(CartV2.createItem('수박', 8000));
 shoppingCartV2.addItem(CartV2.createItem('사과', 12000));
