da40fb99f6654397b85c74e1369261db06a17c76
SteveShim <steveshim496@gmail.com>
2023-02-11 14:59:46 +0900

    Add comment

--- react/2.2/app.js
+++ react/2.2/app.js
@@ -1,9 +1,15 @@
 
 function createDOM(node) {
+
+    // 하위 요소가 태그일 땐 객체로 들어오는데
+    // 태그가 아니면 그냥 문자열로 들어옴
+    console.log("node",node)
     if (typeof node === 'string') {
       return document.createTextNode(node);
     }
-  
+    
+    // DOM을 만들기 위해서는 DOM API를 써야한다
+    // 태그를 만드는 API는 document.createElement
     const element = document.createElement(node.tag);
   
     node.children
@@ -13,41 +19,47 @@ function createDOM(node) {
     return element;
   }
   
-  const vdom = {
-    tag: 'p',
-    props: {},
-    children: [
-      {
-        tag: 'h1',
-        props: {},
-        children: ["React 만들기"],
-      },
-      {
-        tag: 'ul',
-        props: {},
-        children: [
-          {
-            tag: 'li',
-            props: {},
-            children: ["첫 번째 아이템"]
-          },
-          {
-            tag: 'li',
-            props: {},
-            children: ["두 번째 아이템"]
-          },
-          {
-            tag: 'li',
-            props: {},
-            children: ["세 번째 아이템"]
-          },
-        ]
-      }
-    ],
-  };
-  
-  document
-    .querySelector('#root')
-    .appendChild(createDOM(vdom));  
+const vdom = {
+  tag: 'p',
+  props: {},
+  children: [
+    {
+      tag: 'h1',
+      props: {},
+      children: ["React 만들기"],
+    },
+    {
+      tag: 'ul',
+      props: {},
+      children: [
+        {
+          tag: 'li',
+          props: {},
+          children: ["첫 번째 아이템"]
+        },
+        {
+          tag: 'li',
+          props: {},
+          children: ["두 번째 아이템"]
+        },
+        {
+          tag: 'li',
+          props: {},
+          children: ["세 번째 아이템"]
+        },
+      ]
+    },
+    {
+      tag: 'h2',
+      props: {},
+      children: ["Seung Hyuk"],
+    },
+  ],
+};
+
+// 생성된 DOM객체를 UI에 꽂는다
+document
+  .querySelector('#root')
+  .appendChild(createDOM(vdom));  
   
   
