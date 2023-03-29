3b6c4b620c7eeeff18a74bc9c0fe9fe0eb75b402
steve-shim <steveshim496@gmail.com>
2023-03-29 18:15:19 +0900

    python__call__

---
+++ python/calltest.py
@@ -0,0 +1,23 @@
+class Stuff(object):
+
+    def __init__(self, x, y, range):
+        super(Stuff, self).__init__()
+        self.x = x
+        self.y = y
+        self.range = range
+
+    def __call__(self, x, y):
+        self.x = x
+        self.y = y
+        print(f"__call__ with ({self.x}, {self.y})")
+
+    def __del__(self):
+        del self.x
+        del self.y
+        del self.range
+
+s = Stuff(1, 2, 3)
+print("s.x",s.x)
+
+s(7, 8)
+print("s.x",s.x)
